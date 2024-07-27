1c59403f2448db8ab2e9e9a7b611586760ab24e7
Purva Patel <144143336+Purvapatel4725@users.noreply.github.com>
2024-07-27 07:32:55 -0400

    Delete Palindrome_Checker.py

--- Palindrome_Checker.py
+++
@@ -1,9 +0,0 @@
-def functionToFindIfTheWordIsPalindrom(word):
-    reversedWord = word[::-1]
-    if reversedWord == word:
-        return True
-    else:
-        return False
-
-print(functionToFindIfTheWordIsPalindrom("aibohphobia"))
-
